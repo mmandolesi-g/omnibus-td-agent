8699eccd58d1a700c5715faeb905903ca8332b0d
Marcelo Mandolesi <mmandolesi@gannett.com>
2017-06-15 13:31:53 -0400

    Adding CI files

---
+++ ci/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e -u -x
+
+mkdir -p /opt/td-agent /var/cache/omnibus
+yum install which git rpm-build -y
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+export PATH="/usr/local/rvm/bin:/usr/local/rvm/rubies/ruby-2.4.0/bin:${PATH}"
+gem install bundle
+cd omnibus-td-agent
+bundle install --binstubs
+bin/gem_downloader core_gems.rb
+bin/gem_downloader plugin_gems.rb
+bin/gem_downloader ui_gems.rb
+bin/omnibus build td-agent2
+ls -hal pkg/
