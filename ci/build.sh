9ba4feedec38e43fc94381c7c816bc31fbfb6abf
Marcelo Mandolesi <mmandolesi@gannett.com>
2017-06-15 13:39:33 -0400

    Adding CI files

--- ci/build.sh
+++ ci/build.sh
@@ -8,7 +8,6 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 export PATH="/usr/local/rvm/bin:/usr/local/rvm/rubies/ruby-2.4.0/bin:${PATH}"
 gem install bundle
-cd omnibus-td-agent
 bundle install --binstubs
 bin/gem_downloader core_gems.rb
 bin/gem_downloader plugin_gems.rb
